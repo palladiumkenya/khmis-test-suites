05ee60d582589537ae46721ca01ed4e21d73fcc5
andrinem <addymurugi@gmail.com>
2020-03-26 23:57:20 +0300

    Adding test suite

---
+++ Test Suites/New_Patient_enrollment.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New_Patient_enrollment</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>0bbe479c-3f49-4c26-bb90-e6a9bc59dbef</testSuiteGuid>
+</TestSuiteEntity>
