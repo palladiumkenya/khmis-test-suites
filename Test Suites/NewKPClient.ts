55379a565202889a75183e10e0a7d599f662aca8
andrinem <addymurugi@gmail.com>
2020-04-01 10:18:25 +0300

    Amend Unique ID

--- Test Suites/NewKPClient.ts
+++ Test Suites/NewKPClient.ts
@@ -10,4 +10,10 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>c565c906-87a8-460f-a064-ca548a1adb33</testSuiteGuid>
+   <testCaseLink>
+      <guid>b7517753-b035-459b-8c9a-f7cf55119441</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New_KP_Client</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
