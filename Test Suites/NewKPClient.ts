76b96d2bc75c2565fe4114ca345e84b418910d24
andrinem <addymurugi@gmail.com>
2020-04-01 10:16:33 +0300

    KPnewClient

---
+++ Test Suites/NewKPClient.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>NewKPClient</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>c565c906-87a8-460f-a064-ca548a1adb33</testSuiteGuid>
+</TestSuiteEntity>
